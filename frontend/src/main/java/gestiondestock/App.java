e61629b36179f3cbea3255071928f313a335d755
mouadaeh <m.aitelhachmi7790@uca.ac.ma>
2025-11-06 17:26:37 +0100

    starting with stages

--- frontend/src/main/java/gestiondestock/App.java
+++ frontend/src/main/java/gestiondestock/App.java
@@ -1,23 +1,45 @@
 package gestiondestock;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class App extends Application    
 {
-    @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-        primaryStage.setScene(new Scene(loader.load()));
-        primaryStage.show();
-    }
-
-    public static void main( String[] args )
+    public static void main( String[] args)
     {
         launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root= new Group();
+        Scene scene = new Scene(root,Color.LIGHTSKYBLUE);
+        Image logo= new Image("/photos/logo.png");
+        stage.getIcons().add(logo);
+        stage.setTitle("Gestion de stock");
+        stage.setWidth(1000);
+        stage.setHeight(600);
+        // stage.setX(1);
+        // stage.setY(1);
+        //stage.setFullScreen(true);
+
+        stage.setResizable(false);
+        
+
+        Text text=new Text();
+        text.setText("Welcome to the Gestion App");
+        text.setX(150);
+        text.setY(70);
+        root.getChildren().add(text);
+        text.setFont(Font.font("Arial",FontWeight.BOLD,50));
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
