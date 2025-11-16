400b3835cb28f4ac5e6ad662a719d471b7927d16
MeryemAlMoumi <meryemalmoumi1@gmail.com>
2025-11-16 16:37:37 +0100

    Revert "save local work before switching branch"
    This reverts commit 887b9c07be56ec765892bad3224da5bb7bdcca6f.

--- frontend/src/main/java/gestiondestock/app/Welcome.java
+++ frontend/src/main/java/gestiondestock/app/Welcome.java
@@ -14,9 +14,8 @@ public class Welcome extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/StockView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Gestion de Commande");
         primaryStage.show();
